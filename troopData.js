9b61b7100800b946206f13e2e87ca1287bb5ab84
divineShake <nb4fbccr76@privaterelay.appleid.com>
2026-07-07 06:52:28 +0000

    add troopDate.js

---
+++ troopData.js
@@ -0,0 +1,71 @@
+const TROOP_DATA = {
+
+  Defender: {
+
+    name: "ディフェンダー",
+
+    counter: "Attacker",
+
+    tiers: [
+
+      { tier: 1, power: 3, attack: 1, defense: 5, hp: 6, lethality: 1, load: 108 },
+
+      { tier: 2, power: 4, attack: 2, defense: 6, hp: 7, lethality: 2, load: 124 },
+
+      { tier: 3, power: 6, attack: 3, defense: 7, hp: 11, lethality: 3, load: 142 },
+
+      { tier: 4, power: 11, attack: 4, defense: 10, hp: 12, lethality: 4, load: 164 },
+
+      { tier: 5, power: 16, attack: 8, defense: 11, hp: 13, lethality: 7, load: 188 },
+
+      { tier: 6, power: 24, attack: 9, defense: 12, hp: 14, lethality: 8, load: 217 },
+
+      { tier: 7, power: 34, attack: 10, defense: 14, hp: 15, lethality: 9, load: 249 },
+
+      { tier: 8, power: 47, attack: 11, defense: 15, hp: 16, lethality: 10, load: 287 },
+
+      { tier: 9, power: 62, attack: 12, defense: 16, hp: 17, lethality: 11, load: 330 },
+
+      { tier: 10, power: 83, attack: 13, defense: 17, hp: 18, lethality: 12, load: 379 },
+
+      { tier: 11, power: 100, attack: 15, defense: 18, hp: 20, lethality: 14, load: 400 }
+
+    ]
+
+  },
+
+  Attacker: {
+
+    name: "アタッカー",
+
+    counter: "Ranger",
+
+    tiers: [
+
+      { tier: 1, power: 3, attack: 5, defense: 2, hp: 2, lethality: 5, load: 108 },
+
+      { tier: 2, power: 4, attack: 6, defense: 3, hp: 3, lethality: 6, load: 124 },
+
+      { tier: 3, power: 8, attack: 7, defense: 4, hp: 4, lethality: 11, load: 142 },
+
+      { tier: 4, power: 12, attack: 11, defense: 5, hp: 5, lethality: 12, load: 164 },
+
+      { tier: 5, power: 17, attack: 12, defense: 9, hp: 8, lethality: 13, load: 188 },
+
+      { tier: 6, power: 26, attack: 13, defense: 10, hp: 9, lethality: 14, load: 217 },
+
+      { tier: 7, power: 37, attack: 15, defense: 11, hp: 10, lethality: 15, load: 249 },
+
+      { tier: 8, power: 51, attack: 16, defense: 12, hp: 11, lethality: 16, load: 287 },
+
+      { tier: 9, power: 67, attack: 17, defense: 13, hp: 12, lethality: 17, load: 330 },
+
+      { tier: 10, power: 88, attack: 18, defense: 14, hp: 13, lethality: 18, load: 379 },
+
+      { tier: 11, power: 106, attack: 20, defense: 16, hp: 15, lethality: 20, load: 400 }
+
+    ]
+
+  }
+
+};
